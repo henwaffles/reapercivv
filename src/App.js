20d32943c4da32c3a67bf19142c5bae7990fa9f5
HenWaffles <68599838+henwaffles@users.noreply.github.com>
2022-04-10 15:59:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
          <Heading ml="8" size="lg" fontWeight='semibold' color="#f7f7f7">Reaper <br></br> Civilization</Heading>
       <Spacer></Spacer>
       <Button size="md" fontWeight='semibold' color='#000000' backgroundColor="#f7f7f7" mr={3} onClick={()=> window.open("http://www.reapercraft.ga:8125")}>Harita</Button>
-      <Button size="md" fontWeight='semibold' color='#000000' backgroundColor="#f7f7f7" onClick={()=> window.open("https://download1322.mediafire.com/vce0e1z1oxug/j84ozb28o96tuol/mods.zip")}>Modları İndir</Button>
+      <Button size="md" fontWeight='semibold' color='#000000' backgroundColor="#f7f7f7" onClick={()=> window.open("https://download1586.mediafire.com/1p7tkyhxlyhg/7wmadnhisacvx73/mods.zip")}>Modları İndir</Button>
       <IconButton  backgroundColor="#f7f7f7" color='#000000' isRound={true} ml={3} icon={<FaDiscord></FaDiscord>} onClick={()=> window.open("https://discord.gg/reaper")}></IconButton>
       </Flex>
       </VStack>
